91076ba4af4e3a09f36b96057083ff9952cb5d7c
dorcastan <dorcas.tann@gmail.com>
2019-12-04 00:51:09 +0800

    Add: Resizable input fields and propTypes
    Resize add/edit input fields so that the full task can be seen without
    scrolling (until it reaches the defined max width).
    
    Fix some linting errors regarding prop types and code style.

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,188 +1,205 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './TaskList.scss';
 
-function EditTaskForm(props) {
-	return (
-		<form onSubmit={props.onSubmit}>
-			<input
-				className="TaskList-form"
-				type='text'
-				value={props.value}
-				onChange={props.onChange}
-			/>
-		</form>
-	);
+function getResizeWidth(string) {
+  return `${string.length * 0.75}em`; // TODO: find a better way to resize
 }
 
+function EditTaskForm(props) {
+  return (
+    <form className="TaskList-editform" onSubmit={props.onSubmit}>
+      <input
+        className="TaskList-forminput"
+        type="text"
+        value={props.value}
+        onChange={props.onChange}
+        style={{ width: getResizeWidth(props.value) }}
+      />
+    </form>
+  );
+}
+
+EditTaskForm.propTypes = {
+  value: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
 function ListItem(props) {
-	const editSymbol = "\u270e"; // Unicode lower right pencil
-	const deleteSymbol = "X";
-	const editButtonStyle = props.value.isBeingEdited ? "TaskList-listbutton-selected" : "TaskList-listbutton";
+  const editSymbol = '\u270e'; // Unicode lower right pencil
+  const deleteSymbol = 'X';
+  const editButtonStyle = props.value.isBeingEdited ? 'TaskList-listbutton-selected' : 'TaskList-listbutton';
 
-	let elementToDisplay;
-	if (props.value.isBeingEdited) {
-		elementToDisplay = <EditTaskForm
-			value={props.value.currentValue}
-			onChange={props.handleChangeEdit}
-			onSubmit={props.handleSubmitEdit}
-		/>;
-	} else {
-		elementToDisplay = <label>{props.value.value}</label>;
-	}
+  let elementToDisplay;
+  if (props.value.isBeingEdited) {
+    elementToDisplay = <EditTaskForm
+      value={props.value.currentValue}
+      onChange={props.handleChangeEdit}
+      onSubmit={props.handleSubmitEdit}
+    />;
+  } else {
+    elementToDisplay = <span>{props.value.value}</span>;
+  }
 
-	return (
-		<li>
-			<span className="TaskList-listitem">
-				{elementToDisplay}
-				<span>
-					<button className={editButtonStyle} onClick={props.onClickEdit}>
-						{editSymbol}
-					</button>
-					<button className="TaskList-listbutton" onClick={props.onClickDelete}>
-						{deleteSymbol}
-					</button>
-				</span>
-			</span>
-		</li>
-	)
+  return (
+    <li>
+      <span className="TaskList-listitem">
+        {elementToDisplay}
+        <span>
+          <button className={editButtonStyle} onClick={props.onClickEdit}>
+            {editSymbol}
+          </button>
+          <button className="TaskList-listbutton" onClick={props.onClickDelete}>
+            {deleteSymbol}
+          </button>
+        </span>
+      </span>
+    </li>
+  );
 }
 
 function NewTaskForm(props) {
-	return (
-		<form onSubmit={props.onSubmit}>
-			<label>
-				New task:&ensp;
-				<input // TODO: Make this resizable (non-trivial)
-					className="TaskList-form"
-					type='text'
-					value={props.value}
-					onChange={props.onChange}
-				/>
-			</label>
-		</form>
-	);
+  return (
+    <form
+      className="TaskList-newform"
+      onSubmit={props.onSubmit}
+    >
+      <input
+        className="TaskList-forminput"
+        type="text"
+        placeholder="Add a new task"
+        value={props.value}
+        onChange={props.onChange}
+        style={{ width: getResizeWidth(props.value) }}
+      />
+    </form>
+  );
 }
 
 class TaskList extends React.Component {
-	defaultFormValue = "";
-
-	constructor(props) {
+  constructor(props) {
     super(props);
+    this.defaultFormValue = '';
     this.state = {
-    	formValue: this.defaultFormValue,
-      items: JSON.parse(localStorage.getItem('allTasks')) || props.items
+      formValue: TaskList.defaultFormValue,
+      items: JSON.parse(localStorage.getItem('allTasks')) || props.items,
     };
 
     this.handleChangeAdd = this.handleChangeAdd.bind(this);
     this.handleSubmitAdd = this.handleSubmitAdd.bind(this);
     this.handleChangeEdit = this.handleChangeEdit.bind(this);
     this.handleSubmitEdit = this.handleSubmitEdit.bind(this);
-	}
+  }
 
-	saveState() {
-		const items = this.state.items.map(item => this.resetItem(item));
-		localStorage.setItem('allTasks', JSON.stringify(items));
-	}
+  saveState() {
+    const items = this.state.items.map((item) => this.resetItem(item));
+    localStorage.setItem('allTasks', JSON.stringify(items));
+  }
 
-	resetItem(item) {
-		return {
-			...item,
-			isBeingEdited: false,
-			currentValue: item.value
-		}
-	}
+  resetItem(item) {
+    return {
+      ...item,
+      isBeingEdited: false,
+      currentValue: item.value,
+    };
+  }
 
-	handleChangeAdd(event) {
-		this.setState({
-			formValue: event.target.value
-		});
-	}
+  handleChangeAdd(event) {
+    this.setState({
+      formValue: event.target.value,
+    });
+  }
 
-	handleSubmitAdd(event) {
-		const newItem = {
-			id: new Date().getTime(),
-			value: this.state.formValue,
-			isBeingEdited: false,
-			currentValue: this.state.formValue
-		};
-		this.setState({
-			formValue: this.defaultFormValue,
-			items: [...this.state.items, newItem]
-		}, () => this.saveState());
-		event.preventDefault();
-	}
+  handleSubmitAdd(event) {
+    const newItem = {
+      id: new Date().getTime(),
+      value: this.state.formValue,
+      isBeingEdited: false,
+      currentValue: this.state.formValue,
+    };
+    this.setState({
+      formValue: this.defaultFormValue,
+      items: [...this.state.items, newItem],
+    }, () => this.saveState());
+    event.preventDefault();
+  }
 
-	editItem(index, modifier) {
-		const items = this.state.items.slice();
-		const oldItem = items[index];
-		const newItem = modifier(oldItem);
-		items.splice(index, 1, newItem);
-		this.setState({
-			items: items
-		}, () => this.saveState());
-	}
+  editItem(index, modifier) {
+    const items = this.state.items.slice();
+    const oldItem = items[index];
+    const newItem = modifier(oldItem);
+    items.splice(index, 1, newItem);
+    this.setState({
+      items,
+    }, () => this.saveState());
+  }
 
-	handleChangeEdit(index, event) {
-		this.editItem(index, oldItem => {
-			return {
-				...oldItem,
-				currentValue: event.target.value
-			}
-		})
-	}
+  handleChangeEdit(index, event) {
+    this.editItem(index, (oldItem) => ({
+      ...oldItem,
+      currentValue: event.target.value,
+    }));
+  }
 
-	handleSubmitEdit(index, event) {
-		this.editItem(index, oldItem => {
-			return {
-				...oldItem,
-				isBeingEdited: false,
-				value: oldItem.currentValue
-			};
-		});
-		event.preventDefault();
-	}
+  handleSubmitEdit(index, event) {
+    this.editItem(index, (oldItem) => ({
+      ...oldItem,
+      isBeingEdited: false,
+      value: oldItem.currentValue,
+    }));
+    event.preventDefault();
+  }
 
-	toggleEditState(index) {
-		this.editItem(index, oldItem => {
-			return {
-				...oldItem,
-				isBeingEdited: !oldItem.isBeingEdited
-			};
-		});
-	}
+  toggleEditState(index) {
+    this.editItem(index, (oldItem) => ({
+      ...oldItem,
+      isBeingEdited: !oldItem.isBeingEdited,
+    }));
+  }
 
-	deleteItem(index) {
-		const items = this.state.items.slice();
-		items.splice(index, 1);
-		this.setState({
-			items: items,
-		}, () => this.saveState());
-	}
+  deleteItem(index) {
+    const items = this.state.items.slice();
+    items.splice(index, 1);
+    this.setState({
+      items,
+    }, () => this.saveState());
+  }
 
   render() {
-		const listItems = this.state.items.map((item, index) =>
-			<ListItem
-				key={item.id}
-				value={item}
-				onClickEdit={() => this.toggleEditState(index)}
-				onClickDelete={() => this.deleteItem(index)}
-				handleChangeEdit={event => this.handleChangeEdit(index, event)}
-				handleSubmitEdit={event => this.handleSubmitEdit(index, event)}
-			/>);
-		return (
-			<div className="TaskList">
-				<header className="TaskList-header">
-					My To-Dos
-				</header>
-				<ol className="TaskList-list">{listItems}</ol>
-				<NewTaskForm
-					onChange={this.handleChangeAdd}
-					onSubmit={this.handleSubmitAdd}
-					value={this.state.formValue}
-				/>
-			</div>
-		);
-	}
+    const listItems = this.state.items.map((item, index) => (
+      <ListItem
+        key={item.id}
+        value={item}
+        onClickEdit={() => this.toggleEditState(index)}
+        onClickDelete={() => this.deleteItem(index)}
+        handleChangeEdit={(event) => this.handleChangeEdit(index, event)}
+        handleSubmitEdit={(event) => this.handleSubmitEdit(index, event)}
+      />
+    ));
+    return (
+      <div className="TaskList">
+        <header className="TaskList-header">
+          My To-Dos
+        </header>
+        <ol className="TaskList-list">{listItems}</ol>
+        <NewTaskForm
+          onChange={this.handleChangeAdd}
+          onSubmit={this.handleSubmitAdd}
+          value={this.state.formValue}
+        />
+      </div>
+    );
+  }
 }
+
+TaskList.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    value: PropTypes.string.isRequired,
+    isBeingEdited: PropTypes.bool.isRequired,
+    currentValue: PropTypes.string.isRequired,
+  })).isRequired,
+};
 
 export default TaskList;
